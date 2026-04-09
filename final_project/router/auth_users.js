13971bac163c95b7380955830eff883468c4ee93
caleboppong <maroppcal@hotmail.it>
2026-04-09 06:22:06 -0400

    Completed add or modify book review route

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -42,7 +42,24 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!review) {
+    return res.status(400).json({ message: "Review text is required" });
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review successfully added/updated",
+    reviews: books[isbn].reviews
+  });
 });
 
 module.exports.authenticated = regd_users;
