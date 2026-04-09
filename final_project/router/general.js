83b0a8cab1ae6741be0dee2fdda319fbb55f298e
caleboppong <maroppcal@hotmail.it>
2026-04-09 06:13:47 -0400

    Completed registered user login route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,42 +27,47 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (book) {
-    return res.status(200).json(book);
-  }
-
-  return res.status(404).json({ message: "Book not found" });
-});
+    const isbn = req.params.isbn;   // get ISBN from request params
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book);
+    }
+  
+    return res.status(404).json({ message: "Book not found" });
+  });
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author.toLowerCase();
+    const author = req.params.author.toLowerCase();
+  
+    const bookKeys = Object.keys(books);
+  
+    const filteredBooks = bookKeys
+      .filter(key => books[key].author.toLowerCase() === author)
+      .reduce((result, key) => {
+        result[key] = books[key];
+        return result;
+      }, {});
+  
+    if (Object.keys(filteredBooks).length > 0) {
+      return res.status(200).json(filteredBooks);
+    }
+  
+    return res.status(404).json({ message: "No books found for this author" });
+  });
 
-  const filteredBooks = Object.keys(books)
-    .filter(isbn => books[isbn].author.toLowerCase() === author)
-    .reduce((result, isbn) => {
-      result[isbn] = books[isbn];
-      return result;
-    }, {});
-
-  if (Object.keys(filteredBooks).length > 0) {
-    return res.status(200).json(filteredBooks);
-  }
-
-  return res.status(404).json({ message: "No books found for this author" });
-});
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title.toLowerCase();
 
-  const filteredBooks = Object.keys(books)
-    .filter(isbn => books[isbn].title.toLowerCase() === title)
-    .reduce((result, isbn) => {
-      result[isbn] = books[isbn];
+  const bookKeys = Object.keys(books);
+
+  const filteredBooks = bookKeys
+    .filter(key => books[key].title.toLowerCase() === title)
+    .reduce((result, key) => {
+      result[key] = books[key];
       return result;
     }, {});
 
@@ -75,7 +80,7 @@ public_users.get('/title/:title', function (req, res) {
 
 // Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
+  const isbn = req.params.isbn;   // get ISBN from params
   const book = books[isbn];
 
   if (book) {
@@ -84,5 +89,22 @@ public_users.get('/review/:isbn', function (req, res) {
 
   return res.status(404).json({ message: "Book not found" });
 });
+
+//Request for Registering User
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+  
+    if (!isValid(username)) {
+      return res.status(404).json({ message: "User already exists" });
+    }
+  
+    users.push({ username: username, password: password });
+    return res.status(200).json({ message: "User successfully registered. Now you can login" });
+  });
 
 module.exports.general = public_users;
